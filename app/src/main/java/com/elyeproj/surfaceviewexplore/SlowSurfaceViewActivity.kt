9c85137ed2b849000266130825f44ca0359b6a02
elye <elye.project@gmailc.om>
2021-11-15 00:29:02 +1100

    Initial commit

---
+++ app/src/main/java/com/elyeproj/surfaceviewexplore/SlowSurfaceViewActivity.kt
@@ -0,0 +1,12 @@
+package com.elyeproj.surfaceviewexplore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SlowSurfaceViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slow_surfaceview)
+    }
+}
